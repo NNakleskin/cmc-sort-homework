b94e17d3bc1625c2bd72f10da7aceb157a69e40b
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-02-09 22:07:20 +0300

    Creating a repo

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  sort_homework
+//
+//  Created by Никита Наклескин on 09.02.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
