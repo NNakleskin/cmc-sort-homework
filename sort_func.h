5f142d0409cbd2b7714908f00cf99ac8854787cf
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-02-18 13:25:29 +0300

    Create sort_func.h with qsort and bubble sort

---
+++ sort_func.h
@@ -0,0 +1,58 @@
+void bubble_sort(int* a, int n, int* count) // Bubble algorithm (read README file)
+{
+    int c;
+    int count_cmp = 0, count_swap = 0; // counter of swaps and comparisons
+    for(int j = 0; j < n - 1; j++)
+    {
+        for(int i = 0; i < n - 1; i++)
+        {
+            count_cmp++; // count of comparisons ++
+            if(abs(a[i]) < abs(a[i + 1]))
+            {
+                count_swap++; // count of swaps ++
+                c = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = c;
+            }
+        }
+    }
+    count[0] = count_cmp; // save the number of comparisons
+    count[1] = count_swap; // save the nubber of swaps
+}
+
+
+void qsort_alg(int* a, int first, int last, int count[2]) // qsort algorithm (read README file)
+{
+    if(last > first)
+    {
+        int count_cmp = 0, count_swap = 0; // counter of swaps and comparisons
+        int left = first, right = last;
+        int mid = abs(a[(left + right) / 2]);
+        while(left < right)
+        {
+            while(abs(a[left]) > mid)
+            {
+                count_cmp++;
+                left++;
+            }
+            while(abs(a[right]) < mid)
+            {
+                count_cmp++;
+                right--;
+            }
+            if(left <= right)
+            {
+                count_swap++;
+                int c = a[left];
+                a[left] = a[right];
+                a[right] = c;
+                left++;
+                right--;
+            }
+        }
+        count[0] += count_cmp;
+        count[1] += count_swap;
+        qsort_alg(a, first, right, count);
+        qsort_alg(a, left, last, count);
+    }
+}
