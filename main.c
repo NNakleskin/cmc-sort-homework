b4791f95908c94b2799acd139925c3557abd1174
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-04-13 16:14:12 +0300

    The program structure has been changed in accordance with the requirements of the task

--- main.c
+++ main.c
@@ -7,52 +7,19 @@
 #include "sort_func.h"
 
 
-int main(void) {
-    FILE* res = fopen("data/results.txt", "w");
+int main(void)
+{
+    FILE* res = fopen("data/results.txt", "a");
+    fprintf(res, "\n\n\n\n\n");
     int* count = calloc(2, sizeof(int));
     int n; // number of numbers to sort
     printf("Print n = ");
     scanf("%d", &n);
+    fprintf(res, "%d\n", n);
     gen_rand(n); // generate files
     gen_sorted(n);
     gen_rev(n);
-    // BUBBLE SORT RANDOM VALUES
-    FILE* input_1_1 = fopen("data/data1.txt", "r"); // input random values from the file
-    FILE* output_1_1 = fopen("data/output1.1.txt", "w");  // output file for bubble sort random input
     long long* a = (long long*) malloc(n * sizeof(long long));
-    for(int i = 0; i < n; i++)  // scan data from input file
-    {
-        fscanf(input_1_1, "%lld", &a[i]);
-    }
-    fprintf(res, "\nBubble sort random values\n");
-    bubble_sort(a, n, count);
-    fprintf(res, "Count_cmp = %d\nCount_swap = %d\n", count[0], count[1]);// Print count of swaps and comparisons to res.txt
-    count[0] = 0; // zeroing the counters
-    count[1] = 0; //
-    for(int i = 0; i < n; i++) // print data to output file
-    {
-        fprintf(output_1_1, "%lld\n", a[i]);
-    }
-    fclose(input_1_1);
-    fclose(output_1_1);
-    // QUICK SORT RANDOM VALUES
-    FILE* input_1_2 = fopen("data/data1.txt", "r"); // input random values from the file
-    FILE* output_1_2 = fopen("data/output1.2.txt", "w");  // output file for qsort sort random input
-    for(int i = 0; i < n; i++)  // scan data from input file
-    {
-        fscanf(input_1_2, "%lld", &a[i]);
-    }
-    fprintf(res, "\n\nQuick sort random values\n");
-    qsort_alg(a, 0, n, count);
-    fprintf(res, "Count_cmp = %d\nCount_swap = %d\n", count[0], count[1]); // Print count of swaps and comparisons to res.txt
-    count[0] = 0; // zeroing the counters
-    count[1] = 0; //
-    for(int i = 0; i < n; i++)
-    {
-        fprintf(output_1_2, "%lld\n", a[i]);
-    }
-    fclose(input_1_2);
-    fclose(output_1_2);
     // BUBBLE SORT SORTED VALUES
     FILE* input_2_1 = fopen("data/data2.txt", "r"); // input sorted n to 1 file
     FILE* output_2_1 = fopen("data/output2.1.txt", "w"); // output for sorted file
@@ -123,6 +90,78 @@ int main(void) {
     {
         fprintf(output_3_2, "%lld\n", a[i]);
     }
+    // BUBBLE SORT RANDOM VALUES
+    FILE* input_1_1 = fopen("data/data1.txt", "r"); // input random values from the file
+    FILE* output_1_1 = fopen("data/output1.1.txt", "w");  // output file for bubble sort random input
+    for(int i = 0; i < n; i++)  // scan data from input file
+    {
+        fscanf(input_1_1, "%lld", &a[i]);
+    }
+    fprintf(res, "\nBubble sort random values\n");
+    bubble_sort(a, n, count);
+    fprintf(res, "Count_cmp = %d\nCount_swap = %d\n", count[0], count[1]);// Print count of swaps and comparisons to res.txt
+    count[0] = 0; // zeroing the counters
+    count[1] = 0; //
+    for(int i = 0; i < n; i++) // print data to output file
+    {
+        fprintf(output_1_1, "%lld\n", a[i]);
+    }
+    fclose(input_1_1);
+    fclose(output_1_1);
+    // QUICK SORT RANDOM VALUES
+    FILE* input_1_2 = fopen("data/data1.txt", "r"); // input random values from the file
+    FILE* output_1_2 = fopen("data/output1.2.txt", "w");  // output file for qsort sort random input
+    for(int i = 0; i < n; i++)  // scan data from input file
+    {
+        fscanf(input_1_2, "%lld", &a[i]);
+    }
+    fprintf(res, "\n\nQuick sort random values\n");
+    qsort_alg(a, 0, n, count);
+    fprintf(res, "Count_cmp = %d\nCount_swap = %d\n", count[0], count[1]); // Print count of swaps and comparisons to res.txt
+    count[0] = 0; // zeroing the counters
+    count[1] = 0; //
+    for(int i = 0; i < n; i++)
+    {
+        fprintf(output_1_2, "%lld\n", a[i]);
+    }
+    fclose(input_1_2);
+    fclose(output_1_2);
+    // BUBBLE SORT RANDOM VALUES TEST 2
+    FILE* input_1_3 = fopen("data/data1.txt", "r"); // input random values from the file
+    FILE* output_1_3 = fopen("data/output1.3.txt", "w");  // output file for bubble sort random input
+    for(int i = 0; i < n; i++)  // scan data from input file
+    {
+        fscanf(input_1_3, "%lld", &a[i]);
+    }
+    fprintf(res, "\nBubble sort random values test 2\n");
+    bubble_sort(a, n, count);
+    fprintf(res, "Count_cmp = %d\nCount_swap = %d\n", count[0], count[1]);// Print count of swaps and comparisons to res.txt
+    count[0] = 0; // zeroing the counters
+    count[1] = 0; //
+    for(int i = 0; i < n; i++) // print data to output file
+    {
+        fprintf(output_1_3, "%lld\n", a[i]);
+    }
+    fclose(input_1_3);
+    fclose(output_1_3);
+    // QUICK SORT RANDOM VALUES TEST 2
+    FILE* input_1_4 = fopen("data/data1.txt", "r"); // input random values from the file
+    FILE* output_1_4 = fopen("data/output1.4.txt", "w");  // output file for qsort sort random input
+    for(int i = 0; i < n; i++)  // scan data from input file
+    {
+        fscanf(input_1_4, "%lld", &a[i]);
+    }
+    fprintf(res, "\n\nQuick sort random values test 2\n");
+    qsort_alg(a, 0, n, count);
+    fprintf(res, "Count_cmp = %d\nCount_swap = %d\n", count[0], count[1]); // Print count of swaps and comparisons to res.txt
+    count[0] = 0; // zeroing the counters
+    count[1] = 0; //
+    for(int i = 0; i < n; i++)
+    {
+        fprintf(output_1_4, "%lld\n", a[i]);
+    }
+    fclose(input_1_4);
+    fclose(output_1_4);
     fclose(input_3_2);
     fclose(output_3_2);
     fclose(res);
